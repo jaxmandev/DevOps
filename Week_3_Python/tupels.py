b26be12606248f994fc194dd26c111e2c4b809e6
jaxmandev <39777721+jaxmandev@users.noreply.github.com>
2020-11-03 15:40:39 +0000

    Create tupels.py

---
+++ Week_3_Python/tupels.py
@@ -0,0 +1,10 @@
+### Tupels
+# Immutable, cannot be changed
+# Syntax: (var1, var2, var3)
+
+# Create an example tuple
+short_list = "paracetamol", "xanax", "vitamin-c", "eggs", "bread"
+
+# short_list[0] = "hello"
+# Error item does not support assignment
+short_list[0]  # => paracetamol
