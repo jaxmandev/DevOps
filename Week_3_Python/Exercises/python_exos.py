451fa589d47d52047ccda0513e1fb55c09008ddb
Jax <main@Jaxs-MacBook-Pro.local>
2020-11-03 11:10:57 +0000

    .py exercises added

---
+++ Week_3_Python/Exercises/python_exos.py
@@ -0,0 +1,14 @@
+USER INFO
+
+#3 Personal variables
+
+first_name = input(str()).capitalize()
+middle_name = input(str()).capitalize()
+last_name = input(str()).capitalize()
+address = input(str())
+postcode = input(str())
+NI = input(str(upper()))
+
+#2 Professional varibales
+course = input(title())
+education = input(str()).capitalize()
